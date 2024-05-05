97700c4096dad504eec62b065cf9b02884dd45ba
Stephen Meycelino Fransisco <stephenmeycelinowork88@gmail.com>
2024-05-05 22:24:15 +0700

    ✨ add: Shop cart functionality with state in context

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -8,16 +8,30 @@ const StoreContextProvider = (props) => {
   const [token, setToken] = useState("")
   const [food_list, setFood_List] = useState([]);
 
-  const addToCart = (itemId) => {
+  const addToCart = async (itemId) => {
     if (!cartItems[itemId]) {
       setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
     } else {
       setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
     }
+    if (token) {
+      await axios.post(
+        url + "/api/cart/add",
+        { itemId },
+        { headers: { token } }
+      );
+    }
   };
 
-  const removeFromCart = (itemId) => {
+  const removeFromCart = async (itemId) => {
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+    if (token) {
+      await axios.post(
+        url + "/api/cart/remove",
+        { itemId },
+        { headers: { token } }
+      );
+    }
   };
 
   const getTotalCartAmount = () => {
@@ -28,19 +42,29 @@ const StoreContextProvider = (props) => {
         totalAmount += itemInfo.price * cartItems[item];
       }
     }
-		return totalAmount
+    return totalAmount;
   };
 
   const fetchFoodList = async () => {
-    const response = await axios.get(url+"/api/food/list")
-    setFood_List(response.data.data)
-  }
+    const response = await axios.get(url + "/api/food/list");
+    setFood_List(response.data.data);
+  };
+
+  const loadCartData = async (token) => {
+    const response = await axios.post(
+      url + "/api/cart/get",
+      {},
+      { headers: { token } }
+    );
+    setCartItems(response.data.cartData)
+  };
 
   useEffect(()=> {
     async function loadData() {
       await fetchFoodList()
       if (localStorage.getItem("token")) {
         setToken(localStorage.getItem("token"))
+        await loadCartData(localStorage.getItem("token"))
       }
     }
     loadData()
